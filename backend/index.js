f684000314fb8ddc96ee901ae25ee0fe697a38eb
AkshayUpadhyay2411 <75607973+AkshayUpadhyay2411@users.noreply.github.com>
2023-07-19 01:38:03 +0530

    gpt-4 API added

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 import user from "./Routes/user.js";
 import post from "./Routes/post.js";
 import comment from "./Routes/comment.js";
--- backend/test.js
+++ backend/test.js
@@ -1,34 +1,42 @@
-import axios from 'axios';
-async function generateText(prompt) {
-  try {
-    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
-      prompt: prompt,
-      max_tokens: 100,
-      temperature: 0.7,
-      n: 1
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer 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' // Replace with your OpenAI API key
-      }
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: "sk-NlJknUIKrI3NVGDRDb19C37f289441E4982fDa6dEaD3FbC0",
+  basePath : "https://chattyapi.tech/v1"
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function generateCodeExplanation() {
+
+    const response = await openai.createChatCompletion({
+    model: "gpt-4",
+    messages: [
+        {
+        "role": "system",
+        "content": "You will be provided with a piece of code, and your task is to explain it in a concise way."
+        },
+        {
+        "role": "user",
+        "content": "print('Hello World')"
+        }
+    ],
+    temperature: 0,
+    max_tokens: 64,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
     });
 
-    const { choices } = response.data;
-    const generatedText = choices[0].text.trim();
-
-    return generatedText;
-  } catch (error) {
-    console.error('Error:', error.response.data.error);
-    throw error;
-  }
+    // console.log(response.data.choices[0].message.content);
+    return response.data.choices[0].message.content
 }
 
-// Usage example
-const prompt = "Once upon a time";
-generateText(prompt)
-  .then(generatedText => {
-    console.log('Generated text:', generatedText);
+generateCodeExplanation()
+  .then((result) => {
+    console.log("Generated code explanation:", result);
   })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+  .catch((error) => {
+    console.error("An error occurred:", error);
+});
+
