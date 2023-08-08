6661dc8bbe201b5a8ceb73ad0f749607f50da9c9
AkshayUpadhyay2411 <75607973+AkshayUpadhyay2411@users.noreply.github.com>
2023-08-08 13:41:47 +0530

    email-validator issue

--- backend/middleware/verify_user_details.js
+++ backend/middleware/verify_user_details.js
@@ -4,7 +4,7 @@ import { validate } from "deep-email-validator";
 export const verifyUserDetails = async (req, res, next) => {
   const { firstName, lastName, username, password, codeforcesHandle } =
     req.body;
-  console.log(req.body);
+  console.log("Req body verifyUserDetails: " , req.body);
   if (
     username.includes("--") ||
     password.includes("--") ||
@@ -14,13 +14,16 @@ export const verifyUserDetails = async (req, res, next) => {
   ) {
     return res.status(403).send({ message: "invalid credentials" });
   }
-  try {
-    const email_verification_result = await validate(username);
-    if (!email_verification_result.valid) {
-      return res.status(401).send({ message: "Invalid email" });
-    }
-  } catch (error) {
-    return res.status(501).send({ message: "Internal server error" });
-  }
+  console.log("Req body verifyUserDetails 2 : " , req.body);
+  // try {
+  //   const email_verification_result = await validate(username);
+  //   if (!email_verification_result.valid) {
+  //     return res.status(401).send({ message: "Invalid email" });
+  //   }
+  // } 
+  // catch (error) {
+  //   return res.status(501).send({ message: "Internal server error" });
+  // }
+  console.log("Req body verifyUserDetails 3: " , req.body);
   next();
 };
