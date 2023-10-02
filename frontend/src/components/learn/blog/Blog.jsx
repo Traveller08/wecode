1da9830670b132d0fd8c392aa852990238bd886f
Pratham das <prathamdas003@gmail.com>
2023-10-02 21:21:10 +0530

    (style): blogpage redesign complete

--- frontend/src/components/learn/blog/Blog.jsx
+++ frontend/src/components/learn/blog/Blog.jsx
@@ -1,17 +1,23 @@
-import React from "react";
-import "./Blog.css";
+import React from 'react';
+import './Blog.css';
 
 const Blog = ({ title, content }) => {
   return (
     <div className="blog-card">
-      <div className="blog-header">
-        <b>{title}</b>
+      <div className="blog-header ">
+        <h4 className="mb-2" style={{ height: '87px' }}>
+          {title.length <= 35 ? title : title.slice(0, 35)}...
+        </h4>
       </div>
-      <div className="blog-content">
-        <a href={content} target="_blank" rel="noopener noreferrer">
-          Read more
-        </a>
-      </div>
+
+      <a href={content} target="_blank" rel="noopener noreferrer">
+        <button
+          type="submit"
+          className="bg-primary rounded text-white border-0 fw-bold"
+        >
+          Read More
+        </button>
+      </a>
     </div>
   );
 };
--- frontend/src/pages/BlogPage.jsx
+++ frontend/src/pages/BlogPage.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import Blog from "../components/learn/blog/Blog";
-import { BlogData } from "../data/blogData";
-import "./BlogPage.css";
+import React from 'react';
+import Blog from '../components/learn/blog/Blog';
+import { BlogData } from '../data/blogData';
+import './BlogPage.css';
 
 const BlogPage = ({ blogList }) => {
   return (
-    <div className="blog-page">
-      <h1 className="page-title">All Blogs</h1>
+    <div className="blog-page container">
+      <h1 className="page-title fw-bold fs-1" style={{ letterSpacing: 3 }}>
+        All Blogs
+      </h1>
       <div className="blog-list">
         {BlogData.map((b, index) => (
           <Blog key={index} title={b.title} content={b.content} />
