a92364519a99b089fe9cd4bd8eb19d96f3f7dc1b
Bidexdablitz <bamideledamilola3@gmail.com>
2023-10-06 21:46:37 +0100

    Display cached problem indicies to prevent api bug

--- frontend/src/components/practice/contests/ContestCard.jsx
+++ frontend/src/components/practice/contests/ContestCard.jsx
@@ -1,95 +1,73 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
 import Badge from "react-bootstrap/Badge";
 import Stack from "react-bootstrap/Stack";
 import apiService from "../../../services/apiService";
+import axios from "axios";
+
 const ContestCard = (props) => {
-  const handleSolve = (contest) => {
-    const url = `https://codeforces.com/contest/${props.contest.id}`;
-    console.log(url);
-    window.open(url);
-  };
-
-  const handleVirtual = () => {
-    const url = `https://codeforces.com/contestRegistration/${props.contest.id}/virtual/true`;
-    console.log(url);
-    window.open(url);
-  };
-  const handleProblem = (e) => {
-    // console.log(e.target.innerHTML)
-    const url = `https://codeforces.com/contest/${props.contest.id}/problem/${e.target.innerHTML}`;
-    window.open(url);
-  };
-  const [problems, setProblems] = useState([]);
-  useState(() => {
-    const fetchContestProblems = async () => {
-      try {
-        const response = await apiService.getContestProblems(
-          props.contest.id,
-          "voyager_08"
-        );
-        let temp = [];
-        response.data.problems.forEach((problem) => {
-          let bg = "secondary";
-          if (response.data.verdicts[problem.index] === "accepted") {
-            bg = "success";
-          }
-
-          if (response.data.verdicts[problem.index] === "wrong answer") {
-            bg = "danger";
-          }
-          temp.push({
-            index: problem.index,
-            verdict: response.data.verdicts[problem.index],
-            bg: bg,
-          });
-        });
-        console.log(temp);
-        setProblems(temp);
-      } catch (error) {
-        console.log("Error fetching contest problems:", error);
-      }
+    const handleSolve = (contest) => {
+        const url = `https://codeforces.com/contest/${props.contest.id}`;
+        console.log(url);
+        window.open(url);
     };
-    fetchContestProblems();
-  }, []);
-  return (
-    <div className="c-card">
-      <div className="c-name w-100">{props.contest.name}</div>
-      <Stack direction="horizontal" className="c-problems-container" gap={1}>
-        {problems.map((problem) => {
-          return (
-            <Badge
-              bg={problem.bg}
-              pindex={problem.index}
-              className="p-badge"
-              onClick={handleProblem}
-            >
-              {problem.index}
-            </Badge>
-          );
-        })}
-      </Stack>
-      <div className="d-flex justify-content-end c-btn-container w-100">
-        <Button
-          className="c-btn"
-          variant="outline-success"
-          onClick={handleSolve}
-        >
-          solve
-        </Button>
 
-        <Button
-          className="c-btn"
-          variant="outline-success"
-          onClick={handleVirtual}
-        >
-          start virtual
-        </Button>
-      </div>
-    </div>
-  );
+    const handleVirtual = () => {
+        const url = `https://codeforces.com/contestRegistration/${props.contest.id}/virtual/true`;
+        console.log(url);
+        window.open(url);
+    };
+    const handleProblem = (e) => {
+        // console.log(e.target.innerHTML)
+        const url = `https://codeforces.com/contest/${props.contest.id}/problem/${e.target.innerHTML}`;
+        window.open(url);
+    };
+
+    function getBg(problem) {
+        let bg = "secondary";
+
+        props.submissions?.forEach((submission) => {
+            if (submission.problem.index === problem.index) {
+                if (submission.verdict === "OK") {
+                    bg = "success";
+                } else {
+                    if (bg != "success") bg = "danger";
+                }
+            }
+        });
+        return bg;
+    }
+
+    return (
+        <div className="c-card">
+            <div className="c-name w-100">{props.contest.name}</div>
+            <Stack direction="horizontal" className="c-problems-container" gap={1}>
+                {props.problems?.map((problem) => {
+                    return (
+                        <Badge
+                            bg={getBg(problem)}
+                            pindex={problem.index}
+                            className="p-badge"
+                            onClick={handleProblem}
+                        >
+                            {problem.index}
+                        </Badge>
+                    );
+                })}
+            </Stack>
+            <div className="d-flex justify-content-end c-btn-container w-100">
+                <Button className="c-btn" variant="outline-success" onClick={handleSolve}>
+                    solve
+                </Button>
+
+                <Button className="c-btn" variant="outline-success" onClick={handleVirtual}>
+                    start virtual
+                </Button>
+            </div>
+        </div>
+    );
 };
 
 export default ContestCard;
