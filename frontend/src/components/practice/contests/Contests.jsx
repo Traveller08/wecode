e06ceded2aecfa836d69545b26e9e781fc009cdd
Bidexdablitz <bamideledamilola3@gmail.com>
2023-10-06 21:46:00 +0100

    Get Problems and submissions, filter then cache

--- frontend/src/components/practice/contests/Contests.jsx
+++ frontend/src/components/practice/contests/Contests.jsx
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect, useState, useTransition } from "react";
 import "./contests.css";
 import ContestCard from "./ContestCard";
+import axios from "axios";
+
 const Contests = (props) => {
-  console.log(props);
-  return (
-    <div className="p-container">
-      {props.contests.map((contest) => {
-        return <ContestCard contest={contest} type={props.contestType} />;
-      })}
-    </div>
-  );
+    const [loading, startTransition] = useTransition();
+    const [submissions, setSubmissions] = useState();
+    const [problems, setProblems] = useState();
+
+    useEffect(() => {
+        const submissions = {};
+        const problems = {};
+
+        (async function () {
+            // Get all the submissions for the current user and store in state
+            const user = "voyager_08";
+
+            const [{ data: submissionsData }, { data: problemsData }] = await Promise.all([
+                axios.get(`https://codeforces.com/api/user.status?handle=${user}&from=1`),
+                axios.get("https://codeforces.com/api/problemset.problems"),
+            ]);
+
+            // Do more loogic to get the user's problems and store in a dictionary for easy access by each ContestCard
+            startTransition(() => {
+                submissionsData.result.forEach((submission) => {
+                    if (submissions[submission.contestId]?.length > 0) {
+                        submissions[submission.contestId].push(submission);
+                    } else {
+                        submissions[submission.contestId] = [submission];
+                    }
+                });
+                problemsData.result.problems.forEach((problem) => {
+                    if (problems[problem.contestId]?.length > 0) {
+                        problems[problem.contestId].push(problem);
+                    } else {
+                        problems[problem.contestId] = [problem];
+                    }
+                });
+                setSubmissions(submissions);
+                setProblems(problems);
+            });
+        })();
+    }, []);
+
+    return (
+        <div className="p-container">
+            {props.contests.map((contest, i) => {
+                return (
+                    <ContestCard
+                        contest={contest}
+                        problems={problems[contest.id]}
+                        submissions={submissions[contest.id]}
+                        type={props.contestType}
+                        key={i}
+                    />
+                );
+            })}
+        </div>
+    );
 };
 
 export default Contests;
